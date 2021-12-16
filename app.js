bc4992f27d50ab351622dce03c2d91a5f453dd97
Abdullah Ghani <abdullah.ghani3@gmail.com>
2021-12-16 10:13:35 +0530

    changes in Profile

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 var app = angular.module('projects', []);
 
 var data = [{
-  name: 'SURVAIDER',
-  url: 'http://survaider.com',
-  description: 'Core memeber in development of the platform',
-}, {
   name: 'ZAREAM',
-  url: 'https://zaream.greyphase.xyz/',
+  url: 'https://zaream.xyz/',
   description: 'A platform to cluster similar news together using web scrapping and ML. ',
 }, {
   name: 'VANTAGE NEWS',
   url: 'https://drive.google.com/file/d/1kIjwYbFhYO2vYokmW19fV18VtPuKEWyO/view?usp=sharing',
   description: 'A mobile application of news made using Flutter.',
-}, ];
+},];
 
 var skills = [{
-    name: 'JAVASCRIPT (MEAN STACK)',
-    level: 85
-  }, {
-    name: 'PHP (LAMP STACK)',
-    level: 80
-  }, {
-    name: 'DJANGO',
-    level: 50
-  }, {
-    name: 'c++, java, python',
-    level: 70
-  },
+  name: 'JAVASCRIPT (MEAN STACK)',
+  level: 85
+}, {
+  name: 'PHP (LAMP STACK)',
+  level: 80
+}, {
+  name: 'DJANGO',
+  level: 50
+}, {
+  name: 'c++, java, python',
+  level: 70
+},
 
 
 ];
@@ -38,7 +34,7 @@ var funs = [
   'Python',
 ]
 
-app.controller("project_controller", function($scope) {
+app.controller("project_controller", function ($scope) {
   $scope.projs = data;
   $scope.skill = skills;
   $scope.funs = funs;

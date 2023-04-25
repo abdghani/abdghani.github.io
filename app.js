ce0b12f2649690659d693f520126a227560d87ba
abdullah <abdullah.ghani3@gmail.com>
2023-04-25 11:52:33 +0530

    updates

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var app = angular.module('projects', []);
 
 var data = [{
-  name: 'ZAREAM',
+  name: 'Zaream',
   url: 'https://zaream.xyz/',
   description: 'A platform to cluster similar news together using web scrapping and ML. ',
-}, {
-  name: 'VANTAGE NEWS',
-  url: 'https://drive.google.com/file/d/1kIjwYbFhYO2vYokmW19fV18VtPuKEWyO/view?usp=sharing',
-  description: 'A mobile application of news made using Flutter.',
-},];
+},{
+  name: 'My Lease',
+  url: 'https://mylease.co.in/',
+  description: 'A platform for leasing cars to corporate and individuals',
+},{
+  name: 'Happy Dogs',
+  url: 'https://v2.happydogs.in/',
+  description: 'A complete day care solution, with onboarding , booking, diet and games for pet dogs',
+}
+// {
+//   name: 'VANTAGE NEWS',
+//   url: 'https://drive.google.com/file/d/1kIjwYbFhYO2vYokmW19fV18VtPuKEWyO/view?usp=sharing',
+//   description: 'A mobile application of news made using Flutter.',
+// }
+];
 
 var skills = [{
   name: 'JAVASCRIPT (MEAN STACK)',
